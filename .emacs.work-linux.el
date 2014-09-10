6b9b1bb66c548b1db9fee53477531ed2172a7d37
Jeremiah <jeremiah.perry@gmail.com>
2014-09-10 17:34:10 -0500

    add work-linux file

---
+++ .emacs.work-linux.el
@@ -0,0 +1,10 @@
+;;;
+;;; .emacs.work.el
+;;;
+
+(defvar my-location 'work-linux "Where I am.")
+
+(load "~/.emacs.d/general-settings.el")
+;(load "~/.emacs.d/my-packages.el")
+
+(desktop-save-mode t)
