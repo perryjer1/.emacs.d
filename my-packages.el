1cfefa7802eddd754f5569cf22760c06225a5905
Jeremiah Perry <jeremiah.perry@gmail.com>
2014-11-05 12:35:03 -0600

    add r history function

--- my-packages.el
+++ my-packages.el
@@ -98,6 +98,23 @@
 		  (lambda ()
 			(setq ess-default-style 'GNU)))
 
+;; modified from comint-dynamic-list-input-ring
+(defun jer/r-history ()
+  "Display R history."
+  (interactive)
+  (if (or (not (ring-p comint-input-ring))
+		  (ring-empty-p comint-input-ring))
+      (message "No history")
+    (let ((history nil)
+		  (history-buffer " *R History*")
+		  (conf (current-window-configuration)))
+      ;; We have to build up a list ourselves from the ring vector.
+      (dotimes (index (ring-length comint-input-ring))
+		(push (ring-ref comint-input-ring index) history))
+	  (with-output-to-temp-buffer history-buffer
+		(with-current-buffer standard-output
+		  (mapcar (lambda (x) (insert x) (insert "\n")) history))))))
+
 
 ;; Slime
 (require 'slime)
