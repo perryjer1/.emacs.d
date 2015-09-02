a909e501c5f55035dbbdf8b8a83c28a1f90f56e8
Jeremiah Perry <jeremiah.perry@gmail.com>
2015-09-01 21:51:29 -0500

    New init-slime

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -23,17 +23,6 @@
 ;; (global-set-key (kbd "C-*") 'highlight-symbol-at-point)
 
 
-;; ;; Slime
-;; (jp/install-if-needed 'slime)
-;; (require 'slime)
-
-;; ;; http://common-lisp.net/project/slime/doc/html/Installation.html#Installation
-;; (require 'slime-autoloads)
-;; (setq inferior-lisp-program "C:/sbcl/sbcl.exe")
-;; (add-to-list 'slime-contribs 'slime-fancy)
-;; (add-to-list 'slime-contribs 'slime-repl)
-
-
 ;;  https://github.com/ananthakumaran/monky
 (jp/install-if-needed 'monky)
 (require 'monky)
