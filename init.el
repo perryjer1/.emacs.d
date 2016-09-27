7d0146ae51ef79ab629fd316b656304dea188572
Jeremiah Perry <jeremiah.perry@gmail.com>
2016-09-27 14:24:32 -0500

    Emacs 25 requires this....I guess?

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 ;;; init.el -- starting point for user initialization
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+;; I call package-initialize after I set the `package-archives'
+;; variable.
+
 (require 'org)
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
